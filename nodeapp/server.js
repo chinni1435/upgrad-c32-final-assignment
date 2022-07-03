df1fc43d9aa893661ad67517cdacfa561aa6d072
chinni1435 <48010995+chinni1435@users.noreply.github.com>
2022-07-03 09:10:21 +0530

    Update server.js

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/app', (req, res) => {
-  res.send('<h2 style="color: purple"> Hello, World!! <h2>');
+  res.send('<h2 style="color: purple"> Hello, C-32 <h2>');
 });
 
 app.listen(PORT, HOST);
